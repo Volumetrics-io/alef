43e9b0d4232399381a4d0fb8463035a67a9ddacc
Grant Forrest <gaforres@gmail.com>
2025-01-14 11:26:51 -0500

    cleanup homepage a bit

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { LogoutButton } from '@/components/auth/LogoutButton';
 import { useMe } from '@/services/publicApi/userHooks';
-import { Box, Heading } from '@alef/sys';
+import { Box, Hero, Main, NavBar } from '@alef/sys';
 import { Link, useNavigate } from '@verdant-web/react-router';
 import { useEffect } from 'react';
 
@@ -14,16 +14,16 @@ const HomePage = () => {
 	}, [session?.emailVerifiedAt, navigate]);
 
 	return (
-		<Box stacked>
-			<Heading level={1}>Home</Heading>
-			<Link to="/login">Login</Link>
-			{session && (
-				<div>
-					Hi, {session.name}
-					<LogoutButton />
-				</div>
-			)}
-		</Box>
+		<Main>
+			<NavBar>
+				<NavBar.Start>Alef</NavBar.Start>
+				<NavBar.End>{session ? <LogoutButton /> : <Link to="/login">Login</Link>}</NavBar.End>
+			</NavBar>
+			<Hero>
+				<Hero.Title>Home</Hero.Title>
+			</Hero>
+			<Box constrained>{session && <div>Hi, {session.name}</div>}</Box>
+		</Main>
 	);
 };
 

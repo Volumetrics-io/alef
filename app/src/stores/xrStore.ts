19941bc7883526071f2177786f411c915dac6672
Michael Gutensohn <michael@gutensohn.me>
2025-01-16 14:55:37 -0800

    rolled back react & r3f versions, enabled touchPointer.

--- app/src/stores/xrStore.ts
+++ app/src/stores/xrStore.ts
@@ -5,23 +5,12 @@ export const xrStore: XRStore = createXRStore({
 	depthSensing: true,
 	bounded: false,
 	hand: {
-		touchPointer: false,
-		grabPointer: false,
-		rayPointer: {
-			// rayModel: {
-			// 	color: (pointer) => (pointer.getButtonsDown().size > 0 ? 'hsl(240, 100%, 70%)' : 'white'),
-			// 	opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 1.0 : 0.4),
-			// },
-			// cursorModel: {
-			// 	color: colors.primary.value,
-			// 	opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 0.6 : 0.4),
-			// },
+		touchPointer: {
+			cursorModel: {
+				color: colors.primary.value,
+				opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 0.6 : 0.4),
+			},
 		},
-		model: false,
-	},
-	controller: {
-		teleportPointer: false,
-		grabPointer: false,
 		rayPointer: {
 			rayModel: {
 				color: (pointer) => (pointer.getButtonsDown().size > 0 ? 'hsl(240, 100%, 70%)' : 'white'),
@@ -32,6 +21,22 @@ export const xrStore: XRStore = createXRStore({
 				opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 0.6 : 0.4),
 			},
 		},
+		grabPointer: false,
+		model: false,
+	},
+	controller: {
+		rayPointer: {
+			rayModel: {
+				color: (pointer) => (pointer.getButtonsDown().size > 0 ? 'hsl(240, 100%, 70%)' : 'white'),
+				opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 1.0 : 0.4),
+			},
+			cursorModel: {
+				color: colors.primary.value,
+				opacity: (pointer) => (pointer.getButtonsDown().size > 0 ? 0.6 : 0.4),
+			},
+		},
+		teleportPointer: false,
+		grabPointer: false,
 		model: false,
 	},
 });

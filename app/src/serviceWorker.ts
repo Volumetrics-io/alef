58189f0ce37020a7afaba0e87f675d859f9d58a0
Grant Forrest <gaforres@gmail.com>
2025-02-21 12:00:27 -0500

    add service worker with gltf cache

--- app/src/serviceWorker.ts
+++ app/src/serviceWorker.ts
@@ -29,18 +29,17 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(new NavigationRoute(createHandlerBoundToURL('/index.html')));
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
+// Cache model files as they are fetched
 registerRoute(
 	// Add in any other file extensions or routing criteria as needed.
-	({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+	({ url }) => url.origin === import.meta.env.VITE_PUBLIC_API_ORIGIN && url.pathname.endsWith('.gltf'),
 	// Customize this strategy as needed, e.g., by changing to CacheFirst.
 	new StaleWhileRevalidate({
-		cacheName: 'images',
+		cacheName: 'models',
 		plugins: [
 			// Ensure that once this runtime cache reaches a maximum size the
-			// least-recently used images are removed.
-			new ExpirationPlugin({ maxEntries: 50 }),
+			// least-recently used files are removed.
+			new ExpirationPlugin({ maxEntries: 200 }),
 		],
 	})
 );
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,46 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			includeManifestIcons: true,
+			strategies: 'injectManifest',
+			srcDir: 'src',
+			filename: 'serviceWorker.ts',
+			manifest: {
+				id: 'io.alef.app',
+				name: 'Alef',
+				short_name: 'Alef',
+				description: 'Dynamic virtual staging for modern real estate',
+				theme_color: '#000000',
+				background_color: '#ffffff',
+				scope: 'https://app.alef.io/',
+				icons: [
+					{
+						src: '/icons/192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/icons/512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+				categories: [],
+				display: 'standalone',
+				start_url: '/?directLaunch=true',
+			},
+
+			workbox: {
+				sourcemap: true,
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': '/src',

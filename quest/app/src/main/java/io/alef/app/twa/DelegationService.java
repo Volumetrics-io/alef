49ba26344bfb993979c42bee0b89eb5f23578dbd
Grant Forrest <gaforres@gmail.com>
2025-03-03 16:01:24 -0500

    initialize apk

---
+++ quest/app/src/main/java/io/alef/app/twa/DelegationService.java
@@ -0,0 +1,22 @@
+// Portions (c) Meta Platforms, Inc. and affiliates.
+package io.alef.app.twa;
+
+
+import com.meta.androidbrowserhelper.horizonpermissions.PermissionRequestExtraCommandHandler;
+
+import com.meta.androidbrowserhelper.horizonplatformsdk.HorizonPlatformSdkRequestHandler;
+
+
+public class DelegationService extends
+        com.meta.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new PermissionRequestExtraCommandHandler());
+            registerExtraCommandHandler(new HorizonPlatformSdkRequestHandler(getApplicationContext()));
+        
+    }
+}
+

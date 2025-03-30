6c23cb54b459dece8bc2a9d00703bd64ee5715f6
Michael Gutensohn <michael@gutensohn.me>
2025-03-30 11:42:33 -0700

    build clean up.

--- packages/sys/src/components/navMenu/NavMenu.tsx
+++ packages/sys/src/components/navMenu/NavMenu.tsx
@@ -7,9 +7,7 @@ import { Control } from '../control/Control.js';
 import cls from './NavMenu.module.css';
 import { Box, BoxProps } from '../box/Box.js';
 import { Logo } from '../logo/Logo.js';
-import { Button } from '../button/Button.js';
 import { Link, LinkProps } from '@verdant-web/react-router';
-import { XIcon } from 'lucide-react';
 
 export const NavMenuRoot = withClassName(RadixDialog.Root, cls.root);
 export const NavMenuTrigger = withClassName(withProps(RadixDialog.Trigger, { asChild: true }), cls.trigger);
